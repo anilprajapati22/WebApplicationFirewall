f57cd15380bca202d70695e19b0ea80dc5efbb38
anilprajapati <anilprajapati18@gnu.ac.in>
2022-02-23 19:23:27 +0530

    python compatible script

--- iptable-sgn.sh
+++ iptable-sgn.sh
@@ -1,30 +1,34 @@
-echo -e "Enter 1 For Adding in Black List\n2 for Removing from Black List : \n"
-echo -e "3 for Adding in WhiteList for SSH"
-echo -e "4 for Block All SHH connection"
-echo -e "5 for saving All Rules"
-echo -e "6 for Reset All Rules"
-echo -e "7 for Add Customer ip"
-echo -e "8 for Remove Customer ip"
-read choice
+main(){
+    choice=$1
+    if [[ ! -z $2 ]]
+    then
+        ipadd=$2
+        clientIp=$2
+    fi
 
-if [[ "$USER" != "root" ]]
-then 
-    echo -e "you must bo root user to use this script"
-else    
     if [[ 1 == $choice ]]
     then 
-        echo -e "enter Ip Address : "
-        read ipadd
+        if [[ -z $2 ]]
+        then
+            echo -e "enter Ip Address : "
+            read ipadd
+        fi    
         iptables --table mangle -I PREROUTING -s $ipadd -p tcp --dport 80 -j DROP
     elif [[ 2 == $choice ]]
     then 
-        echo -e "enter Ip Address : "
-        read ipadd
+        if [[ -z $2 ]]
+        then
+            echo -e "enter Ip Address : "
+            read ipadd
+        fi    
         iptables --table mangle -D PREROUTING -s $ipadd -p tcp --dport 80 -j DROP
     elif [[ 3 == $choice ]]
     then 
-        echo -e "enter Ip Address : "
-        read ipadd
+        if [[ -z $2 ]]
+        then
+            echo -e "enter Ip Address : "
+            read ipadd
+        fi    
         iptables --table mangle -I PREROUTING -s $ipadd -p tcp --dport 22 -j ACCEPT       
     elif [[ 4 == $choice ]]
     then 
@@ -48,21 +52,54 @@ else
     elif [[ 7 == $choice ]]
     then 
         echo -e "Adding Client Ip : "
-        echo -e "enter Client Ip Address : "
         firewallIp=192.168.1.66
-        read clientIp
+        if [[ -z $2 ]]
+        then
+            echo -e "enter Client Ip Address : "
+            echo -e "enter Ip Address : "
+            read clientIp
+        fi    
         iptables --table nat --append PREROUTING --protocol tcp --destination $firewallIp --dport 80 --jump DNAT --to-destination $clientIp
         iptables --table nat --append POSTROUTING --protocol tcp --destination $clientIp --dport 80 --jump SNAT --to-source $firewallIp
 
     elif [[ 8 == $choice ]]
     then 
         echo -e "Remove Client Ip : "
-        echo -e "enter Client Ip Address : "
+        
         firewallIp=192.168.1.66
-        read clientIp
+        if [[ -z $2 ]]
+        then
+            echo -e "enter Client Ip Address : "
+            echo -e "enter Ip Address : "
+            read clientIp
+        fi    
         iptables --table nat -D PREROUTING --protocol tcp --destination $firewallIp --dport 80 --jump DNAT --to-destination $clientIp
         iptables --table nat -D POSTROUTING --protocol tcp --destination $clientIp --dport 80 --jump SNAT --to-source $firewallIp
 
+    fi    
+}
 
-    fi
+
+if [[ "$USER" != "root" ]]
+then 
+    echo -e "you must bo root user to use this script"
+else    
+    if [[ ! -z $1 ]]
+    then
+        echo -e "command mode\n"
+        main $1 $2
+    else
+        echo -e "CLI mode\n"
+        echo -e "Enter 1 For Adding in Black List\n2 for Removing from Black List : \n"
+        echo -e "3 for Adding in WhiteList for SSH"
+        echo -e "4 for Block All SSH connection"
+        echo -e "5 for saving All Rules"
+        echo -e "6 for Reset All Rules"
+        echo -e "7 for Add Customer ip"
+        echo -e "8 for Remove Customer ip"
+        read choice
+
+        main $choice
+
+    fi        
 fi
